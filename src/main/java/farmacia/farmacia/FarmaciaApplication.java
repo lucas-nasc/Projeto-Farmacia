be4ac2b7f0377bcbffe5560fedca72ccc111aeb5
lucas-nasc <lcsnascimento91@gmail.com>
2023-09-19 13:44:22 -0300

    att1 19/09

--- src/main/java/farmacia/farmacia/FarmaciaApplication.java
+++ src/main/java/farmacia/farmacia/FarmaciaApplication.java
@@ -3,6 +3,7 @@ package farmacia.farmacia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class FarmaciaApplication {
 
